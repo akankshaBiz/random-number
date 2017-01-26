10619821bfa9559aa7ddcea6f5e169be32ce8b35
Akanksha Rawat <akanksha27aug@gmail.com>
2017-01-26 19:28:05 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+var generateRndNum= function(name)
+	{
+		min = Math.ceil(6);
+		max = Math.floor(15);
+		var randomNum= Math.floor(Math.random() * (max - min)) + min;
+		document.getElementById("name").innerHTML = name+randomNum;
+	}
